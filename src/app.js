5f1343e59e062b2b62c513a38ecd04004787f783
Dovydas Venckus <dovydas.venck@gmail.com>
2018-12-01 23:43:25 +0200

    Add aurelia plugin for materialize theme

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Time tracker';
     config.map([
-      { route: ['', 'time-tracker'], name: 'time-tracker',      moduleId: PLATFORM.moduleName('time-tracker'),      nav: true, title: 'Timer'}
+      { route: ['', 'time-tracker'], name: 'time-tracker', moduleId: PLATFORM.moduleName('time-tracker'), nav: true, title: 'Timer' }
     ]);
 
     this.router = router;
