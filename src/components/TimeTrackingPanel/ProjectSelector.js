de434cb91d2ff315fdffc3667b747d4ab56281aa
Dovydas Venckus <dovydas.venck@gmail.com>
2019-06-29 21:19:46 +0300

    Fix sore error in store creation

--- src/components/TimeTrackingPanel/ProjectSelector.js
+++ src/components/TimeTrackingPanel/ProjectSelector.js
@@ -1,23 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ProjectSelector = props => (
-  <div className="select is-fullwidth">
-    <select
-      onChange={props.handleProjectUpdate}
-      value={props.currentProject}
-      disabled={props.isTracking || props.currentProject === "no-id"}
-    >
-      {props.projects.map(project => {
-        return (
-          <option key={project.id} value={project.id}>
-            {project.name}
-          </option>
-        );
-      })}
-    </select>
-  </div>
-);
+const ProjectSelector = props => {
+  const { projects, currentProject, isTracking, handleProjectUpdate } = props;
+  return (
+    <div className="select is-fullwidth">
+      <select
+        onChange={handleProjectUpdate}
+        value={currentProject}
+        disabled={isTracking || currentProject === "no-id"}
+      >
+        {projects.map(project => {
+          return (
+            <option key={project.id} value={project.id}>
+              {project.name}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
 
 ProjectSelector.propTypes = {
   projects: PropTypes.array,
