41aa9cd14f869ec960bb16fd0ca50a5319b30fa7
Dovydas Venckus <dovydas.venck@gmail.com>
2019-09-28 18:22:45 +0300

    Disable add project button when tracking is on

--- src/components/TimeTrackingPanel/ProjectSelector.js
+++ src/components/TimeTrackingPanel/ProjectSelector.js
@@ -12,11 +12,11 @@ const ProjectSelector = props => {
   return (
     <div className="columns is-mobile">
       <div className="column is-1-widescreen is-2-tablet is-1-mobile add-project-button-container">
-        <a onClick={openCreateProjectModal} className="button is-primary">
+        <button className="button is-primary" onClick={openCreateProjectModal} disabled={isTracking}>
           <span className="icon">
             <i className="fa fa-plus" />
           </span>
-        </a>
+        </button>
       </div>
       <div className="column is-11-widescreen is-10-tablet is-11-mobile">
         <div className="select is-fullwidth">
