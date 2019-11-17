818d99a10168753613c901e19853bb2c440ecd44
Dovydas Venckus <dovydas.venck@gmail.com>
2019-11-17 17:49:01 +0200

    Add page not found handler

--- src/TimeTrackerApp.js
+++ src/TimeTrackerApp.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import TimeTrackerPage from './page/TimeTrackerPage';
 import Navbar from './components/Navbar/Navbar';
+import NotFoundPage from './page/NotFoundPage';
 
 const TimeTrackerApp = () => (
   <Router>
@@ -14,6 +15,9 @@ const TimeTrackerApp = () => (
         </Route>
         <Route path="/projects">
           <h1 className="title">Projects</h1>
+        </Route>
+        <Route to="*">
+          <NotFoundPage />
         </Route>
       </Switch>
     </section>
