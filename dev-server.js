be6b2a8858895fa57e711b564afb64f530b506b3
Dovydas Venckus <dovydas.venck@gmail.com>
2019-12-01 16:18:04 +0200

    Update dev server API proxy port

--- dev-server.js
+++ dev-server.js
@@ -1,14 +1,14 @@
-const { createServer } = require("http");
-const { createProxyServer } = require("http-proxy");
-const Path = require("path");
-const Bundler = require("parcel-bundler");
+const { createServer } = require('http');
+const { createProxyServer } = require('http-proxy');
+const Path = require('path');
+const Bundler = require('parcel-bundler');
 
-const backendUrl = "http://localhost:8080";
-const frontendUrl = "http://localhost:1234";
+const backendUrl = 'http://localhost:8180';
+const frontendUrl = 'http://localhost:1234';
 
 const options = {};
 
-const entryFiles = Path.join(__dirname, "src", "index.html");
+const entryFiles = Path.join(__dirname, 'src', 'index.html');
 
 const bundler = new Bundler(entryFiles, options);
 
@@ -17,19 +17,19 @@ bundler.serve();
 const proxy = createProxyServer();
 
 const server = createServer((req, res) => {
-  if (req.url.includes("/api/")) {
+  if (req.url.includes('/api/')) {
     proxy.web(req, res, {
       target: backendUrl,
       changeOrigin: true,
-      autoRewrite: true
+      autoRewrite: true,
     });
   } else {
     proxy.web(req, res, {
       target: frontendUrl,
-      ws: true
+      ws: true,
     });
   }
 });
 
-console.log("Dev proxy listening at: http://localhost:5050/");
+console.log('Dev proxy listening at: http://localhost:5050/');
 server.listen(5050);
