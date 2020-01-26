66e909a0c8d2fe10fc2c1ba377dab80974bf5b59
Dovydas Venckus <dovydas.venck@gmail.com>
2020-01-26 12:39:40 +0200

    Add project page title

--- src/page/ProjectsListPage.js
+++ src/page/ProjectsListPage.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ProjectsListContainer from '../containers/ProjectsListContainer';
 
-const ProjectListPage = () => <ProjectsListContainer />;
+const ProjectListPage = () => (
+  <div>
+    <h1 className="title">Projects</h1>
+    <ProjectsListContainer />
+  </div>
+);
 
 export default ProjectListPage;
