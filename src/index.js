2117646befdebfa050ee1151dbf68f7451a6a4f6
Dovydas Venckus <dovydas.venck@gmail.com>
2020-02-20 21:33:37 +0200

    Add toast success/error messages for archiving/restoring project

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 // eslint-disable-next-line no-unused-vars
 import styles from 'bulma';
 // eslint-disable-next-line no-unused-vars
+import 'react-toastify/dist/ReactToastify.css';
+// eslint-disable-next-line no-unused-vars
 import stylesTooltip from 'bulma-tooltip';
 import '@babel/polyfill';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify/esm/react-toastify';
 import TimeTrackerApp from './TimeTrackerApp';
 import store from './state/store';
 import { AuthProvider } from './utils/auth/authProvider';
@@ -15,6 +18,7 @@ const AppRoot = document.getElementById('app');
 ReactDOM.render(
   <AuthProvider>
     <Provider store={store}>
+      <ToastContainer autoClose={3000} draggable={false} position={'top-center'} />
       <TimeTrackerApp />
     </Provider>
   </AuthProvider>,
